53d68f5470ee0d8ce5f2f1384bfcc141819c9619
Marhime <35594487+Marhime@users.noreply.github.com>
2024-07-08 15:48:58 -0600

    build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
-            publicDirectory: "public_html",
+            publicDirectory: "public_html", // spécifiez votre répertoire public
         }),
     ],
+    build: {
+        outDir: "public_html/build", // spécifiez le répertoire de sortie
+    },
 });
